3de355568bd736d3bd49638cc745307223366ef6
Breno Battaglin <breno.battaglin@icloud.com>
2022-03-07 21:37:42 -0300

    Create storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
-import 'package:bloc/bloc.dart';
 import 'package:cryptocurrency_converter/app.dart';
 import 'package:cryptocurrency_converter/observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_services_binding/flutter_services_binding.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  BlocOverrides.runZoned(
+main() async {
+  FlutterServicesBinding.ensureInitialized();
+  final storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
+
+  HydratedBlocOverrides.runZoned(
     () => runApp(const App()),
     blocObserver: Observer(),
+    storage: storage,
   );
 }
