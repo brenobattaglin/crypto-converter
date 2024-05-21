40fb31baf0701ab224d494161394b3ed5c831f27
Breno Battaglin <breno.battaglin@icloud.com>
2024-05-21 11:51:08 -0300

    fix: update route generator management and initial route

--- lib/presenter/core/routes.dart
+++ lib/presenter/core/routes.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 
 class Routes {
   static const String converter = '/converter';
+  static const String initial = '/';
 }
 
 class RouteManager {
   static Route<MaterialPageRoute> getRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
+      case Routes.initial:
       case Routes.converter:
         return MaterialPageRoute(builder: (_) => const ConversionView());
       default:
