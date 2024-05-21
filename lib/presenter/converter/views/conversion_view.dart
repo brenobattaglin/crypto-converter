1a3ac2786c7909fdcfe259f2a741b22bbb7ad777
Breno Battaglin <breno.battaglin@icloud.com>
2024-05-21 11:53:41 -0300

    fix: update refresh controller
    - refresh was being called before the ui, causing a warning

--- lib/presenter/converter/views/conversion_view.dart
+++ lib/presenter/converter/views/conversion_view.dart
@@ -3,11 +3,9 @@ import 'package:crypto_converter/domain/converter/converter.dart';
 import 'package:crypto_converter/presenter/core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
 import '../converter.dart';
-
-RefreshController _refreshController = RefreshController(initialRefresh: true);
 
 class ConversionView extends StatefulWidget {
   const ConversionView({Key? key}) : super(key: key);
@@ -18,10 +16,12 @@ class ConversionView extends StatefulWidget {
 
 class _ConversionViewState extends State<ConversionView> {
   late Currency selectedCurrency;
+  late RefreshController refreshController;
 
   @override
   void initState() {
     selectedCurrency = SupportedCurrencies.list.entries.first.value;
+    refreshController = RefreshController(initialRefresh: true);
     super.initState();
   }
 
@@ -35,28 +35,25 @@ class _ConversionViewState extends State<ConversionView> {
         child: BlocListener<ConverterCubit, ConverterState>(
           listener: (context, state) {
             if (state.status == ConversionStatus.failure) {
-              _refreshController.refreshFailed();
+              refreshController.refreshFailed();
             } else if (state.status == ConversionStatus.success) {
-              _refreshController.refreshCompleted();
+              refreshController.refreshCompleted();
             }
           },
           child: SmartRefresher(
-            onRefresh: () async {
-              context.read<ConverterCubit>().fetchExchangeRates(selectedCurrency.code);
-            },
+            primary: false,
             enablePullDown: true,
+            onRefresh: _onRefresh,
             header: const WaterDropHeader(),
-            controller: _refreshController,
+            controller: refreshController,
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   BlocBuilder<ConverterCubit, ConverterState>(
-                    builder: (context, state) {
-                      return _buildCryptoCurrencyList(context, state.exchangeRates);
-                    },
+                    builder: (context, state) => _buildCryptoCurrencyList(state.exchangeRates),
                   ),
-                  DropdownButtonWidget(selectedCurrency),
+                  _buildCoinDropdownButton(refreshController: refreshController),
                 ],
               ),
             ),
@@ -66,79 +63,56 @@ class _ConversionViewState extends State<ConversionView> {
     );
   }
 
-  _buildCryptoCurrencyList(BuildContext context, List<ExchangeRate> exchangeRates) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.count(
-          primary: false,
-          crossAxisCount: 2,
+  void _onRefresh() async => context.read<ConverterCubit>().fetchExchangeRates(selectedCurrency.code);
+
+  _buildCryptoCurrencyList(List<ExchangeRate> exchangeRates) => Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            primary: false,
+            crossAxisCount: 2,
+            children: [
+              for (var exchange in exchangeRates)
+                CryptoCardWidget(
+                  code: exchange.cryptocurrency.code,
+                  rate: exchange.rate.toStringAsFixed(2),
+                  name: exchange.cryptocurrency.name,
+                  iconData: exchange.cryptocurrency.icon,
+                )
+            ],
+          ),
+        ),
+      );
+
+  Padding _buildCoinDropdownButton({required RefreshController refreshController}) => Padding(
+        padding: const EdgeInsets.fromLTRB(10, 100, 10, 100),
+        child: Column(
           children: [
-            for (var exchange in exchangeRates)
-              CryptoCardWidget(
-                code: exchange.cryptocurrency.code,
-                rate: exchange.rate.toStringAsFixed(2),
-                name: exchange.cryptocurrency.name,
-                iconData: exchange.cryptocurrency.icon,
-              )
+            Text(
+              'Select the currency:',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            DropdownButtonHideUnderline(
+              child: DropdownButton(
+                dropdownColor: Nord0,
+                value: selectedCurrency,
+                icon: const Icon(Icons.arrow_downward),
+                items: SupportedCurrencies.list.entries
+                    .map((entry) => DropdownMenuItem<Currency>(
+                          value: entry.value,
+                          child: Text(
+                            entry.key,
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ))
+                    .toList(),
+                onChanged: (Currency? newValue) => setState(() {
+                  selectedCurrency = newValue!;
+                  refreshController.requestRefresh(needMove: true);
+                }),
+              ),
+            ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class DropdownButtonWidget extends StatefulWidget {
-  Currency selectedCurrency;
-
-  DropdownButtonWidget(this.selectedCurrency, {Key? key});
-
-  @override
-  _DropdownButtonWidgetState createState() => _DropdownButtonWidgetState();
-}
-
-class _DropdownButtonWidgetState extends State<DropdownButtonWidget> {
-  late Currency currentCurrencySelected;
-  @override
-  void initState() {
-    currentCurrencySelected = widget.selectedCurrency;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 100, 10, 100),
-      child: Column(
-        children: [
-          Text(
-            'Select the currency:',
-            style: Theme.of(context).textTheme.bodySmall,
-          ),
-          DropdownButtonHideUnderline(
-            child: DropdownButton(
-              dropdownColor: Nord0,
-              value: currentCurrencySelected,
-              icon: const Icon(Icons.arrow_downward),
-              items: SupportedCurrencies.list.entries.map((entry) {
-                return DropdownMenuItem<Currency>(
-                  value: entry.value,
-                  child: Text(
-                    entry.key,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                );
-              }).toList(),
-              onChanged: (Currency? newValue) {
-                setState(() {
-                  currentCurrencySelected = newValue!;
-                  _refreshController.requestRefresh(needMove: true);
-                });
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+      );
 }
