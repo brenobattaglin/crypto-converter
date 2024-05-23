376c3141bc4dcb8af07fc117d96e561b79369d89
Breno Battaglin <breno.battaglin@icloud.com>
2024-05-23 15:32:32 -0300

    chore: add default value for env key
    - add default value to avoid issues about .env on ci

--- lib/core/infrastructure/env.dart
+++ lib/core/infrastructure/env.dart
@@ -4,6 +4,6 @@ part 'env.g.dart';
 
 @Envied(path: '.env')
 final class Env {
-  @EnviedField(varName: 'COIN_API_KEY')
+  @EnviedField(varName: 'COIN_API_KEY', defaultValue: 'api_key')
   static const String coinApiKey = _Env.coinApiKey;
 }
