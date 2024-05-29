7377ad2714075bdfc265f9eb11fea98c8e04de86
Breno Battaglin <breno.battaglin@icloud.com>
2024-05-28 21:26:39 -0300

    refactor: create colors class

---
+++ lib/core/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color _nord0 = Color(0xFF2E3440);
+  static const Color _nord1 = Color(0xFF3B4252);
+  static const Color _nord6 = Color(0xFFECEFF4);
+
+  static const Color white = _nord6;
+  static const Color black = _nord0;
+  static const Color grey = _nord1;
+  static const Color transparent = Colors.transparent;
+}
