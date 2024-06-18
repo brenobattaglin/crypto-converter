631ff9cc078a6e565c70d0a464ea78e764bebef3
Breno Battaglin <breno.battaglin@icloud.com>
2024-06-18 01:51:41 -0300

    style: create sizes

---
+++ lib/core/sizes.dart
@@ -0,0 +1,9 @@
+class AppSizes {
+  AppSizes._();
+
+  static const double extraSmall = 4.0;
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double large = 24.0;
+  static const double extraLarge = 32.0;
+}
