a13471e00a9f809686dc1873d113617d5494138a
Breno Battaglin <breno.battaglin@icloud.com>
2024-07-06 00:03:29 -0300

    test: update mocks

--- test/mocks/classes.dart
+++ test/mocks/classes.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:crypto_converter/domain/converter/usecases/list/list_exchange_rate_usecase.dart';
 import 'package:crypto_converter/presenter/converter/cubits/converter_cubit.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:mocktail/mocktail.dart';
@@ -6,3 +7,5 @@ import 'package:mocktail/mocktail.dart';
 class MockStorage extends Mock implements Storage {}
 
 class MockConverterCubit extends MockCubit<ConverterState> implements ConverterCubit {}
+
+class MockListExchangeRateUsecase extends Mock implements ListExchangeRateUsecase {}
--- test/mocks/setup.dart
+++ test/mocks/setup.dart
@@ -1,5 +1,12 @@
+import 'package:crypto_converter/domain/converter/usecases/list/list_exchange_rate_dto.dart';
+import 'package:mocktail/mocktail.dart';
+
 import 'storage.dart';
 
 initAppMocks() {
   mockHydratedStorage();
+}
+
+registerFallbacks() {
+  registerFallbackValue(InputListExchangeRateDto(cryptoCodes: List.empty(), currencyCode: ''));
 }
