830ec089319e4c5796f0868017d3b8b38dfa62de
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2024-11-01 21:57:41 -0300

    fix: update gradle

--- android/app/src/main/kotlin/com/brenobattaglin/crypto_price_tracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/brenobattaglin/crypto_price_tracker/MainActivity.kt
@@ -2,5 +2,4 @@ package com.brenobattaglin.crypto_price_tracker
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
