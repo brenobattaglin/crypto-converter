d7f5accddc90ea9c02c4e3798318bec5a03416ef
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2024-12-02 21:13:05 -0300

    refactor: update imports and try/catches

--- lib/modules/converter/usecases/list_exchange_rate/list_exchange_rate_usecase.dart
+++ lib/modules/converter/usecases/list_exchange_rate/list_exchange_rate_usecase.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
 
-import 'package:crypto_converter/modules/converter/models/exchange_rate.dart';
-import 'package:crypto_converter/modules/converter/usecases/list_exchange_rate/list_exchange_rate_dto.dart';
 import 'package:crypto_converter/core/usecase.dart';
 import 'package:crypto_converter/modules/coin_api/repository/coin_repository.dart';
+import 'package:crypto_converter/modules/converter/models/exchange_rate.dart';
+import 'package:crypto_converter/modules/converter/usecases/list_exchange_rate/list_exchange_rate_dto.dart';
 
 class ListExchangeRateUsecase implements Usecase<InputListExchangeRateDto, OutputListExchangeRateDto> {
   final CoinRepository _coinRepository;
@@ -23,9 +23,9 @@ class ListExchangeRateUsecase implements Usecase<InputListExchangeRateDto, Outpu
       }
 
       return OutputListExchangeRateDto(exchangeRates: output);
-    } catch (e) {
+    } on Object catch (e, stackTrace) {
       //TODO: check better error structure
-      log('[ListExchangeRateUsecase] Error: $e');
+      log('[ListExchangeRateUsecase] Error: $e, stackTrace: $stackTrace', error: e);
       throw Exception();
     }
   }
